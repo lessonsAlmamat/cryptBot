5f38a4242a0ecd6100d866b4eefd4d3e6fcfa63e
git config --global user.email leroyyyyyy@yandex.ru <leroykid02@mail.ru>
2021-07-31 18:50:21 +0300

    Сделали команду для генерации 32-символьных ключей

--- cryptBot.py
+++ cryptBot.py
@@ -1,3 +1,4 @@
+from cryptography.fernet import Fernet
 import telebot
 from os.path import join
 from os import getcwd, getenv
@@ -12,29 +13,48 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
-    bot.reply_to(message, 'Привет! Я бот для шифрования данных!')
+    bot.reply_to(message, 'Привет! Я бот для шифрования данных! /gen - генерация паролей, /createkey - генерация ключа.')
 
 @bot.message_handler(commands=['gen'])
 def generatePasswords(message):
-    filepath = join(getcwd(), 'passwords', 'pass' + str(message.date) + '.txt')
-    passwordGenerator(16, 20, filepath)
-    bot.send_document(chat_id=message.chat.id, data=open(filepath))
+    try:
+        filepath = join(getcwd(), 'passwords', 'pass' + str(message.date) + '.txt')
+        passwordGenerator(16, 20, filepath)
+        bot.send_document(chat_id=message.chat.id, data=open(filepath))
+    except Exception as e:
+        print('Произошла ошибка:', e)
+        bot.reply_to(message, 'Что-то пошло не так! Попробуйте позже!')
+
+@bot.message_handler(commands=['createkey'])
+def generatePasswords(message):
+    try:
+        key = Fernet.generate_key()
+        bot.send_message(chat_id=message.chat.id, text=key)
+    except Exception as e:
+        print('Произошла ошибка:', e)
+        bot.reply_to(message, 'Что-то пошло не так! Попробуйте позже!')
+
 
 @bot.message_handler(content_types=['document'])
 def cryptFile(message):
-    opt = message.caption.split()
+    try:
+        opt = message.caption.split()
 
-    filepath = join(getcwd(), 'files', 'file' + str(message.date) + '.txt')
-    file_info = bot.get_file(message.document.file_id)
-    down_file = bot.download_file(file_info.file_path)
-    with open(filepath, 'wb') as file:
-        file.write(down_file)
+        filepath = join(getcwd(), 'files', 'file' + str(message.date) + '.txt')
+        file_info = bot.get_file(message.document.file_id)
+        down_file = bot.download_file(file_info.file_path)
+        with open(filepath, 'wb') as file:
+            file.write(down_file)
 
-    if opt[1] == '1':
-        encrypt(filepath, opt[0])
-    elif opt[1] == '2':
-        decrypt(filepath, opt[0])
+        if opt[1] == '1':
+            encrypt(filepath, opt[0])
+        elif opt[1] == '2':
+            decrypt(filepath, opt[0])
 
-    bot.send_document(chat_id=message.chat.id, data=open(filepath))
+        bot.send_document(chat_id=message.chat.id, data=open(filepath))
+
+    except Exception as error:
+        print('Произошла ошибка:', error)
+        bot.reply_to(message, 'Что-то пошло не так! Возможно вы отправили неправильные данные!')
 
 bot.polling()
